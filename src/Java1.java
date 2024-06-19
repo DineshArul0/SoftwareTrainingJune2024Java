27cea27cf1112e373db8c59271c147c8f4e80078
DineshArul7 <dinesh.ak37@gmail.com>
2024-06-19 11:56:03 +0530

    First Commit

---
+++ src/Java1.java
@@ -0,0 +1,52 @@
+public class Java1 {
+
+
+    //For auto indendation ctrl+alt+l
+
+    public static void main(String args[]) {
+        Java1 java1 = new Java1();//object creation
+        java1.method1();
+        method2();
+    }
+
+    void method1() {
+        System.out.println("Hello java");
+    }
+
+    static void method2() {
+        System.out.println("Hello world");
+
+    }
+
+    void variables(){
+        String str="";
+        String character="3";
+        str="Deekay";
+        String str2="Dinesh";
+        int a=2;
+        double b=2.4;
+        boolean c=true;
+        boolean d=false;
+        String s=null;
+        char e='#';
+    }
+
+//Variables
+    /*
+    DataTypes
+    byte
+    short
+    int
+    long
+    double
+    float
+    char
+    boolean
+    String
+
+     */
+
+
+
+
+}
