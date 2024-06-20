db1b35280877aa40319f45f6b1a1d0317d027cb5
DineshArul7 <dinesh.ak37@gmail.com>
2024-06-20 12:22:36 +0530

    Day 2 codes

--- src/Java1.java
+++ src/Java1.java
@@ -5,8 +5,13 @@ public class Java1 {
 
     public static void main(String args[]) {
         Java1 java1 = new Java1();//object creation
-        java1.method1();
-        method2();
+//        java1.method1();
+//        method2();
+//        method3("Hello Universe");
+//        method3("Hello Nation");
+//        stringoperations("dinesh","ASDSDFDFEURTRTRdfdfdeebbbSS");
+ //       conditionalopeators();
+        ifelse();
     }
 
     void method1() {
@@ -15,7 +20,9 @@ public class Java1 {
 
     static void method2() {
         System.out.println("Hello world");
-
+    }
+    static  void method3(String str){
+        System.out.println(str);
     }
 
     void variables(){
@@ -31,6 +38,69 @@ public class Java1 {
         char e='#';
     }
 
+    static void stringoperations(String str,String str2){
+        boolean result=str.equals("Dinesh");
+        System.out.println(result);
+        String str1=str2.toLowerCase();
+        System.out.println(str1);
+        char c=str.charAt(4);
+        System.out.println(c);
+        System.out.println(str.charAt(3));
+        String custom="World Greatest Leader";
+        System.out.println(custom.charAt(6));
+        System.out.println(custom.indexOf("G"));
+
+        str.trim();
+        str.strip();
+        str.substring(0,3);
+        str.split("-");
+
+    }
+
+    static void conditionalopeators(){
+        int a=10;
+        int b=12;
+        boolean result=a<b;
+        System.out.println(result);
+        result=a>=b;
+        System.out.println(result);
+        result=a!=b;
+        System.out.println(result);
+        String str="Dinesh";
+        String str2="Dinesh";
+        result=str.equalsIgnoreCase(str2);
+        System.out.println(result);
+
+        /*
+        <
+        >
+        ==
+        <=
+        >=
+        !=
+        int double char String equals or equalsIgnoreCase
+         */
+    }
+
+    static void ifelse(){
+        boolean value=true;
+        double x=12.5;
+        double y=12.4;
+        if(value){
+            System.out.println("the Value is true");
+        }else{
+            System.out.println("the Value is false");
+        }
+        if(x>y){
+            System.out.println("the value of x is greater than y");
+        } else if (x==y) {
+            System.out.println("the value of x is equal to y");
+        }
+       else{
+            System.out.println("the value of x is lesser than y");
+        }
+    }
+
 //Variables
     /*
     DataTypes
@@ -44,6 +114,20 @@ public class Java1 {
     boolean
     String
 
+     */
+
+    //String Operations
+    /*
+    conditional Operators
+    >
+    <
+    ==
+    >=
+    <=
+    !=
+    if else
+    while
+    for loop
      */
 
 
