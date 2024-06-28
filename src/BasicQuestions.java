90463a0ce2358d9d7314786400eee0a6c0af8904
DineshArul0 <dinesh.arul07@gmail.com>
2024-06-28 11:42:25 +0530

    testing the commit

--- src/BasicQuestions.java
+++ src/BasicQuestions.java
@@ -17,6 +17,7 @@ public class BasicQuestions {
         //added just
 
         String str = "aeiou12";
+        //added from test
         int vowelscount = 0;
         for (int i = 0; i < str.length(); i++) {
             char ch = Character.toLowerCase(ch = str.charAt(i));
